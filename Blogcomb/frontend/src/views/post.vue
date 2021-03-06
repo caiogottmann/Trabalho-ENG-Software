695111c6975025e1cf139298aa1d9679737d106b
Miguel <rgomiguel17@gmail.com>
2021-03-06 15:02:36 -0300

    pagina post, usada pra testar a sidebar

---
+++ Blogcomb/frontend/src/views/post.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <sidebar/>
+    <navbar-admin/>
+  </div>
+</template>
+
+<script>
+import NavbarAdmin from '../components/navbarAdmin.vue'
+import Sidebar from '../components/sidebar.vue'
+
+
+
+export default {
+  components:{
+    Sidebar,
+    NavbarAdmin
+  }  
+}
+</script>
+
+<style scoped>
+
+</style>
