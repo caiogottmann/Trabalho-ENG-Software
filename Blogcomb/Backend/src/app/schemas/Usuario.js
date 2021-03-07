79ed5e0792b7faf109f18c83555a8401a5efcbb2
kaisersir <ivan.gazzi.marcal@gmail.com>
2021-03-07 14:19:02 -0300

    schema Usuario

---
+++ Blogcomb/Backend/src/app/schemas/Usuario.js
@@ -0,0 +1,35 @@
+import mongoose from '@/database';
+import bcrypt from 'bcryptjs';
+import crypto from 'crypto';
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    lowercase: true,
+    unique: true,
+  },
+  Admin: {
+    type: Number,
+    default: 1,
+  },
+  password: {
+    type: String,
+    required: false,
+    select: false,
+  },
+});
+
+UserSchema.pre('save', function (next) {
+  this.role = 'user';
+  const hash = bcrypt.hashSync(this.password, 10);
+  this.password = hash;
+  next();
+});
+
+export default mongoose.model('User', UserSchema);
