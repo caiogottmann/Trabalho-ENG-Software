5431d2d9c344a58665fe1bf72ae1c7a1f0ead20e
Caio Gottmann <caiogottmann@gmail.com>
2021-03-07 18:29:09 -0300

    alteracao no schema

--- Blogcomb/Backend/src/app/schemas/Post.js
+++ Blogcomb/Backend/src/app/schemas/Post.js
@@ -1,26 +1,26 @@
-import mongoose from '@/database';
+import mongoose from "@/database";
 
-const emailSchema = new mongoose.Schema({
+const postSchema = new mongoose.Schema({
   titulo: {
     type: String,
     required: true,
   },
   texto: {
     type: String,
-    required: true
+    required: true,
   },
   data: {
     type: Date,
-    required: true
+    required: true,
   },
   tag: {
     type: Array,
-    required: true
+    required: true,
   },
   creator: {
     type: String,
-    required: true
+    required: true,
   },
 });
 
-export default mongoose.model('posts', postSchema);
+export default mongoose.model("posts", postSchema);
