79e7be80cf628c7705095fae5219abfc65adbc9b
Caio Gottmann <caiogottmann@gmail.com>
2021-03-21 15:37:51 -0300

    Login integracao

--- Blogcomb/frontend/src/services/api/Api.js
+++ Blogcomb/frontend/src/services/api/Api.js
@@ -1,10 +1,15 @@
 import axios from "axios";
+import { GetCookie } from "@/utils/CookieUtil";
 
 const url = "http://localhost:5000";
-
 
 export default () => {
   return axios.create({
     baseURL: url,
+    headers: {
+      Authorization: GetCookie("token")
+        ? `Bearer ${GetCookie("token")}`
+        : undefined,
+    },
   });
 };
--- Blogcomb/frontend/src/views/login.vue
+++ Blogcomb/frontend/src/views/login.vue
@@ -4,40 +4,73 @@
     <navbar />
     <div id="bloco">
       <h3>Entrar</h3>
-      <form>
-        <input placeholder="E-mail"/>
-        <input placeholder="Senha"/>
-        <input id="submit" type="submit" value="Entrar"/>
-      </form>
+      <b-form @submit="signIn">
+        <b-form-input
+          type="email"
+          v-model="credentials.email"
+          placeholder="E-mail"
+          required
+        ></b-form-input>
+        <b-form-input
+          type="password"
+          v-model="credentials.password"
+          placeholder="Senha"
+          required
+        ></b-form-input>
+        <b-button type="submit" id="submit">Entrar</b-button>
+      </b-form>
     </div>
+    <modal-feedback
+      @fecha="showError = false"
+      :certo="false"
+      mensagem="Erro no Loginn"
+      :ativado="showError"
+    />
   </div>
 </template>
 
 <script>
-import navbar from '../components/navbar.vue'
+import navbar from "../components/navbar.vue";
+import { AuthLogin } from "@/services/api/Auth.js";
+import { SetCookie } from "../utils/CookieUtil.js";
+import ModalFeedback from "@/components/modalFeedback.vue";
 
-export default{
-  components: { navbar },
-  setup() {
-    
+export default {
+  components: { navbar, ModalFeedback },
+  data() {
+    return { credentials: { email: "", password: "" }, showError: false };
   },
-}
+  methods: {
+    signIn(evt) {
+      evt.preventDefault();
+      AuthLogin(this.credentials)
+        .then((res) => {
+          SetCookie("token", res.data.token, {
+            expires: Number.parseInt(res.data.tokenExpiration),
+          });
+          this.$router.push("/admin");
+        })
+        .catch(() => {
+          this.showError = true;
+        });
+    },
+  },
+};
 </script>
 
-
 <style lang="scss" scoped>
-#tudo{
-  background-color: #F0F0F0;
+#tudo {
+  background-color: #f0f0f0;
   position: absolute;
   width: 100vw;
   height: 100vh;
   z-index: -1;
 }
-#bloco{
+#bloco {
   padding-top: 20px;
   font-family: Lato-Bold;
   width: 30vw;
-  height: 50vh;
+  height: 35vh;
   margin-left: 35vw;
   margin-top: 15vh;
   background-color: #fefefe;
@@ -46,36 +79,33 @@ export default{
   justify-content: space-around;
   align-items: center;
 
-  form{
+  form {
     display: flex;
     flex-direction: column;
     align-items: center;
     width: 70%;
 
-
-    input{
-      border: 2px solid #1B3458;
+    input {
+      border: 2px solid #1b3458;
       border-radius: 5px;
       padding: 12px;
-      margin-bottom:9%;
+      margin-bottom: 9%;
       width: 100%;
     }
-    #submit{
+    #submit {
       padding: 5px;
       width: 100px;
-      background-color: #1B3458;
-      color: #F0F0F0;
+      background-color: #1b3458;
+      color: #f0f0f0;
     }
   }
 }
 
-
 @media (max-width: 750px) {
-  #bloco{
+  #bloco {
     margin-left: 10vw;
     width: 80vw;
     height: 60vh;
   }
 }
-
 </style>
