c2c6ea379afdf34478fcca365850a9ba53e47738
kaisersir <ivan.gazzi.marcal@gmail.com>
2021-03-22 09:40:45 -0300

    Delete posts funcional

--- Blogcomb/Backend/src/app/controllers/Post.js
+++ Blogcomb/Backend/src/app/controllers/Post.js
@@ -11,11 +11,12 @@ const router = new Router();
 router.post('/', authMiddleware, async (req, res) => {
     const { titulo, texto, data, tag } = req.body;
     const creator=req.uid;
+    console.log(tag)
     for (var i = 0; i < tag.length; i++) {
-      var existetag = await Tag.findOne({tag: tag[i].tag})
+      var existetag = await Tag.findOne({tag: tag[i]})
       if(!existetag) {
         Tag.create({
-          tag : tag[i].tag
+          tag : tag[i]
         })
         .catch((error) => {
           console.error(
@@ -31,8 +32,7 @@ router.post('/', authMiddleware, async (req, res) => {
       .catch((error) => {
         console.error(
           'Erro ao criar schema de Post',
-          error, 
-          'titulo: ', titulo, ' texto: ', texto, ' data: ', data, ' tag: ', tag, ' creator: ', creator
+          error
         );
       });
       res.status(200).send('Post cadastrado com sucesso');
@@ -112,39 +112,27 @@ router.get('/:postsId', /*authMiddleware,*/ async (req, res) => {
 router.delete('/:postsId', authMiddleware, async (req, res) => {
   console.log(req.params);
   const Admin  = req.Admin;
-  if (!Admin){
+  var creatorID= 0;
+  var post = "qualquer coisa";
+  await Post.findOne({_id: req.params.postsId}).then((postencontrado) => {
+    creatorID=postencontrado.creator;
+    //console.log(post, "\n", postencontrado, "\n")
+    post = postencontrado;
+    //console.log("mudou?\n", post, "\n", postencontrado, "\n")
+  })
+  .catch((error) => {
+    console.error('Erro ao encontrar o post', error);
+    res.status(400).send(
+      'Não foi possível deletar o post, tente novamente',
+    );
+  });
+  
+  if (!Admin && creatorID!=req.uid){
     return res.status(401).send('Sem autorização para realizar essa ação' );
   }
     await Post
     .findByIdAndRemove(req.params.postsId)
     .then(() => {
-
-      Tag.find()
-      .then((tags) => {
-        for (var i = 0; i < tags.length; i++) {
-            tagscompact = tagscompact.concat({
-              id:posts[i]._id,
-              titulo: posts[i].titulo,
-              texto: posts[i].texto,
-              data: posts[i].data,
-              tag: posts[i].tag,
-              creator: posts[i].creator,
-            });
-          }
-        res.status(200).send(tagscompact);
-      })
-      .catch((error) => {
-        console.error('Erro ao obter os dados', error);
-        res.status(400).send(
-          'Não foi possível exibir os dados, tente novamente',
-        );
-      });
-
-
-
-
-
-
       return res.status(200).send('Removido com sucesso' );
     })
     .catch((error) => {
