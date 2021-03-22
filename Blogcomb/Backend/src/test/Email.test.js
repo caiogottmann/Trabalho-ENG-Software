95dfb21059acac6c925117bac41113d3583119a4
Caio Gottmann <caiogottmann@gmail.com>
2021-03-22 10:51:04 -0300

    Correcao test

--- Blogcomb/Backend/src/test/Email.test.js
+++ Blogcomb/Backend/src/test/Email.test.js
@@ -5,6 +5,10 @@ describe("Testa as rotas relacionadas ao email", () => {
   it("Adicionar email", async () => {
     await request(app)
       .post("/Email")
+      .set(
+        "Authorization",
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI2MDU3OTIwYjk0NzRhMTM4YTRlMzRkNTIiLCJBZG1pbiI6MSwiaWF0IjoxNjE2NDIwODYxLCJleHAiOjE2MTY1MDcyNjF9.GvjOuSOxdYmfkiiom1uKXIymgXP2qNKgd_WBeau-wOY"
+      )
       .send({
         nome: "Nome Usuario",
         email: "nome.usuario@teste.com",
@@ -13,12 +17,22 @@ describe("Testa as rotas relacionadas ao email", () => {
   });
 
   it("Get Emails", async () => {
-    await request(app).get("/Email").expect(200);
+    await request(app)
+      .get("/Email")
+      .set(
+        "Authorization",
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI2MDU3OTIwYjk0NzRhMTM4YTRlMzRkNTIiLCJBZG1pbiI6MSwiaWF0IjoxNjE2NDIwODYxLCJleHAiOjE2MTY1MDcyNjF9.GvjOuSOxdYmfkiiom1uKXIymgXP2qNKgd_WBeau-wOY"
+      )
+      .expect(200);
   });
 
   it("Atualizar email", async () => {
     await request(app)
       .post("/Email/edit")
+      .set(
+        "Authorization",
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI2MDU3OTIwYjk0NzRhMTM4YTRlMzRkNTIiLCJBZG1pbiI6MSwiaWF0IjoxNjE2NDIwODYxLCJleHAiOjE2MTY1MDcyNjF9.GvjOuSOxdYmfkiiom1uKXIymgXP2qNKgd_WBeau-wOY"
+      )
       .send({
         nome: "Novo Nome Usuario",
         email: "nome.usuario@teste.com",
@@ -27,6 +41,12 @@ describe("Testa as rotas relacionadas ao email", () => {
   });
 
   it("Deletar email", async () => {
-    await request(app).delete("/Email/nome.usuario@teste.com").expect(200);
+    await request(app)
+      .delete("/Email/nome.usuario@teste.com")
+      .set(
+        "Authorization",
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI2MDU3OTIwYjk0NzRhMTM4YTRlMzRkNTIiLCJBZG1pbiI6MSwiaWF0IjoxNjE2NDIwODYxLCJleHAiOjE2MTY1MDcyNjF9.GvjOuSOxdYmfkiiom1uKXIymgXP2qNKgd_WBeau-wOY"
+      )
+      .expect(200);
   });
 });
